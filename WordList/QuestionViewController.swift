d33fde94ee210aa6e07527daa2828f7a9988d4f4
Kahototo <johndoe@example.k.t.07242@gmail.com>
2016-06-09 00:06:36 +0900

    first commit

---
+++ WordList/QuestionViewController.swift
@@ -0,0 +1,99 @@
+//
+//  QuestionViewController.swift
+//  WordList
+//
+//  Created by 小西夏穂 on 2016/06/08.
+//  Copyright © 2016年 小西夏穂. All rights reserved.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+    
+    @IBOutlet var nextButton: UIButton!
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    var isAnswered: Bool = false  //回答したか・次の問題に行くのかの判定
+    var worryArray: [AnyObject] = []  //ユーザーデフォルトからとる配列
+    var shuffledWordArray: [AnyObject] = []  //シャッフルされた配列
+    var nowNumber: Int = 0  //現在の回答数
+    
+    let saveData = NSUserDefaults.standardUserDefaults()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        answerLabel.text = ""
+    }
+    
+    //viewが現れた時に呼ばれる
+    override func viewWillAppear(animated: Bool) {
+        worryArray = saveData.arrayForKey("WORD")!
+        //問題をシャッフルする
+        shuffle()
+        questionLabel.text = shuffledWordArray[nowNumber]["english"] as? String
+        
+    }
+    
+     func shuffle() {
+        while worryArray.count > 0
+            let index = Int(rand()) % worryArray.count
+            shuffledWordArray.append(worryArray[index])
+            worryArray.removeAtIndex(index)
+        }
+    
+
+    func nextButtonPushed () {
+        
+        //回答したか
+        if isAnswered {
+            //次の問題へ
+            nowNumber += 1
+            answerLabel.text = ""
+            
+            //次の問題を表示するか
+            if nowNumber < shuffledWordArray.count  {
+            //次の問題を表示
+                questionLabel.text = shuffledWordArray[nowNumber]["english"] as? String
+                //isAnsweredをfalseにする
+                isAnswered = false
+                //ボタンのタイトルを変更する
+                nextButton.setTitle("答えを表示", forState: UIControlState.Normal)
+    } else {
+                //これ以上表示する問題はないので、Finishビューに画面移動
+                self.shouldPerformSegueWithIdentifier("toFinishView", sender: nil)
+        }
+            
+    } else {
+            //答えを表示する
+            answerLabel.text = shuffledWordArray[nowNumber]["japanise"] as? String
+            //isAnswerをtrueにする
+            isAnswered = true
+            //ボタンのタイトルを変更する
+            nextButton.setTitle("次へ", forState: UIControlState.Normal)
+     }
+    }
+    
+    
+    
+        // Do any additional setup after loading the view.
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+
--- WordList/ViewController.swift
+++ WordList/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +20,37 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func back(segue: UIStoryboardSegue){
+        
+    }
+    
+    @IBAction func startButtonTapped() {
+        let saveData = NSUserDefaults.standardUserDefaults()
+        
+        if let wordArray = saveData.arrayForKey("WORD") {
+            if wordArray.count > 0 {
+                self.performSegueWithIdentifier("toQuestionView", sender: nil)
+                return
+            }
+        }
+        
+        let alert: UIAlertController = UIAlertController(
+            title: "単語",
+            message: "まずは『単語一覧』をタップして単語登録してください",
+            preferredStyle: .Alert
+        )
+        
+        alert.addAction(UIAlertAction(
+            title: "OK",
+            style: .Default,
+            handler: nil
+            )
+       )
+       self.presentViewController(alert, animated: true, completion: nil)
+    }
 
-
+    @IBAction func tappedButton(sender: AnyObject) {
+    }
 }
 
